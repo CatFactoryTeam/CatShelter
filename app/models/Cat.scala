7638da052bf9280a53e3257edb98fc379c886f46
Brice Thomas <brice.p.thomas@gmail.com>
2015-04-05 16:15:58 +0200

    Code style improvements and new cat's data :cat2:

--- app/models/Cat.scala
+++ app/models/Cat.scala
@@ -1,8 +1,24 @@
 package models
 
-/** A cat.
+import me.verticale.imgur.model.Image
+
+/** A cat factory -- literally... yep the pattern ;) */
+object Cat {
+
+  /** Create a new cat from the given image
+    *
+    * @param img Image's data
+    *
+    * @return A soft and warm cat
+    */
+  def apply(img: Image) = new Cat(img.id.get, img.link.get, img.mp4.get, img.webm.get)
+}
+
+/** A cat
   *
   * @param id Cat identifier (Imgur id)
-  * @param link Link to the image (Imgur link)
+  * @param link Link to the GIF image
+  * @param mp4 Link to the MP4
+  * @param webm Link to the WebM
   */
-case class Cat(id: String, link: String)
+class Cat(id: String, link: String, mp4: String, webm: String)
--- app/services/CatShelter.scala
+++ app/services/CatShelter.scala
@@ -3,17 +3,16 @@ package services
 import models.Cat
 
 import play.Logger
-import play.api.Play
+import play.api.Play.{configuration, current}
 
 import me.verticale.imgur.Imgur
 
 /** The cat shelter, where cats live and rest peacefully
-  *
   * Handle everything Imgur related
   */
 object CatShelter {
-    lazy val cats = createCats()
-    val imgur = new Imgur(Play.current.configuration.getString("imgur.key").get)
+    val cats = createCats()
+    val imgur = new Imgur(configuration.getString("imgur.key").get)
 
     /** Create all cats from the Imgur album
       *
@@ -22,14 +21,14 @@ object CatShelter {
     def createCats(): Array[Cat] = {
       Logger.info("[ImgurService] Creating cats...")
 
-      var list = Array[Cat]()
-      val images = imgur.albumImages(Play.current.configuration.getString("imgur.album").get)
+      // Retrieve images from the album "imgur.album"
+      // Map each image to create a list with Cat instances
+      val cats = imgur.albumImages(configuration.getString("imgur.album").get)
+        .map(Cat(_))
+        .toArray
 
-      for (image <- images)
-        list = Cat(image.id.get, image.link.get) +: list
+      Logger.info("[ImgurService] Cats created!")
 
-      Logger.info("[ImgurService] Cats created !")
-
-      list
+      cats
     }
 }
